9e8388a469c02db2b1abe1ebf105dbc4a31d6f07
Primoz Cigler <primoz@proteusnet.com>
2015-02-03 14:33:42 +0100

    Added author widget from Readable.

---
+++ widgets/widget-author.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Author box widget
+ *
+ * @package Restaurant
+ */
+
+
+if ( ! class_exists( 'PW_Author' ) ) {
+	class PW_Author extends WP_Widget {
+
+		/**
+		 * Register widget with WordPress.
+		 */
+		public function __construct() {
+			parent::__construct(
+				false, // ID, auto generate when false
+				sprintf( 'ProteusThemes: %s', __( 'Author Widget', 'proteuswidgets' ) ),
+				array(
+					'classname' => 'widget-author'
+				)
+			);
+		}
+
+		/**
+		 * Front-end display of widget.
+		 */
+		public function widget( $args, $instance ) {
+			extract( $args );
+			$selected_user_id = intval( $instance['selected_user_id'] );
+
+			echo $before_widget;
+			?>
+
+				<div class="widget-author__image-container">
+					<div class="widget-author__avatar--blurred">
+						<?php echo get_avatar( $selected_user_id, 90 ); ?>
+					</div>
+					<a href="<?php echo get_author_posts_url( $selected_user_id ); ?>" class="widget-author__avatar">
+						<?php echo get_avatar( $selected_user_id, 90 ); ?>
+					</a>
+				</div>
+				<div class="row">
+					<div class="col-xs-10  col-xs-offset-1">
+						<?php echo $before_title; ?><?php the_author_meta( 'display_name', $selected_user_id ); ?><?php echo $after_title; ?>
+						<?php echo wpautop( get_the_author_meta( 'description', $selected_user_id ) ); ?>
+
+						<?php if ( strlen( get_the_author_meta( 'user_url', $selected_user_id) ) ) : ?>
+						<p>
+							<a href="<?php the_author_meta( 'user_url', $selected_user_id ); ?>"><?php the_author_meta( 'user_url', $selected_user_id ); ?></a>
+						</p>
+						<?php endif ?>
+						<?php
+							$icons = get_social_icons_links( get_user_meta( $selected_user_id ) );
+							if ( count( $icons ) ) {
+								echo '<p class="social-icons__author">';
+							}
+							foreach ( $icons as $service => $url ) {
+								printf( '<a href="%s" class="social-icons__container"><span class="%s"></span></a>', esc_url( $url[0] ), $service );
+							}
+							if ( count( $icons ) ) {
+								echo '</p>';
+							}
+						?>
+					</div>
+				</div>
+
+			<?php
+			echo $after_widget;
+		}
+
+		/**
+		 * Sanitize widget form values as they are saved.
+		 */
+		public function update( $new_instance, $old_instance ) {
+			$instance = array();
+
+			$instance['selected_user_id'] = intval( $new_instance['selected_user_id'] );
+
+			return $instance;
+		}
+
+		/**
+		 * Back-end widget form.
+		 */
+		public function form( $instance ) {
+			$selected_user_id = isset( $instance['selected_user_id'] ) ? $instance['selected_user_id'] : 1;
+
+			?>
+
+			<p>
+				<label for="<?php echo $this->get_field_id( 'selected_user_id' ); ?>"><?php _ex( 'Display author: ', 'readable_wp'); ?></label>
+				<?php wp_dropdown_users( array(
+					'name'     => $this->get_field_name( 'selected_user_id' ),
+					'id'       => $this->get_field_id( 'selected_user_id' ),
+					'selected' => $selected_user_id,
+					'class'    => 'widefat',
+				) ); ?>
+
+			</p>
+
+			<?php
+
+
+		}
+
+	}
+	register_widget( 'PW_Author' );
+}
