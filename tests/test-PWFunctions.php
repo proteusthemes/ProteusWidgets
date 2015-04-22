9e6ab1825a2b8634bcb42d69918856133ae42c65
Gregor Capuder <capuderg@gmail.com>
2015-04-22 14:14:42 +0200

    Added tests for the reorder_widget_array_key_values method.

--- tests/test-PWFunctions.php
+++ tests/test-PWFunctions.php
@@ -65,6 +65,51 @@ class PWFunctionsTest extends WP_UnitTestCase {
 		);
 	}
 
+	/**
+	 * @expectedException PHPUnit_Framework_Error
+	 */
+	function test_reorder_widget_array_key_values_missing_argument() {
+		$this->assertEmpty( PWFunctions::reorder_widget_array_key_values() );
+	}
+
+	function test_reorder_widget_array_key_values() {
+		$is_array_error = false;
+		$reordered_array = array();
+
+		// input array with unordered array keys and a non array value
+		$testing_array = array(
+			'0' => array( 'first' ),
+			'1' => 'second',
+			'2' => array( 'third' ),
+			'4' => new stdClass( 'forth' ),
+			'7' => array( 'fifth' ),
+		);
+
+		// array with ordered array keys
+		$correct_array = array(
+			'0' => array( 'first' ),
+			'1' => array( 'second' ),
+			'2' => array( 'third' ),
+			'3' => array( 'forth' ),
+			'4' => array( 'fifth' ),
+		);
+
+		$reordered_array = PWFunctions::reorder_widget_array_key_values( $testing_array );
+		foreach ( $reordered_array as $array ) {
+			if ( ! is_array( $array ) ) {
+				$is_array_error = true;
+			}
+		}
+
+		$this->assertEquals(
+			array_keys( $reordered_array ),
+			array_keys( $correct_array ),
+			'the array does not have the correct order of its keys'
+		);
+
+		$this->assertFalse( $is_array_error, 'array values are not of type Array' );
+	}
+
 	function test_installed_after() {
 		$this->ProteusWidgets->plugin_activation();
 		$this->assertTrue( PWFunctions::installed_after( '0' ), 'all versions are installed after 0' );
