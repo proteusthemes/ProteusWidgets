4ea007d0bbcebc820c802ee48764c0c151f256a2
Primoz Cigler <primoz@proteusnet.com>
2015-12-04 14:50:25 +0100

    Fixed widget view for widget skype.

--- widgets/views/widget-skype.php
+++ widgets/views/widget-skype.php
@@ -1,8 +1,8 @@
-<?php echo $this->args['before_widget'] ?>
+<?php echo $args['before_widget'] ?>
 
 	<a class="skype-button" href="<?php echo $this->e( $instance['skype_username'], 'esc_attr' ) ?>">
 		<i class="fa  fa-<?php echo $this->e( $instance['icon'], 'esc_attr' ) ?>"></i>
 		<p class="skype-button__title"><?php echo $this->e( $instance['title'], 'wp_kses_post' ) ?></p>
 	</a>
 
-<?php echo $this->args['after_widget'] ?>
+<?php echo $args['after_widget'] ?>
