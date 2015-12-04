230b1a6644145d7b7156ce096f38472cc20f19b8
Gregor Capuder <capuderg@gmail.com>
2015-12-04 16:10:29 +0100

    Updated Number Counter view.

--- widgets/views/widget-number-counter.php
+++ widgets/views/widget-number-counter.php
@@ -1,15 +1,15 @@
-{{{ args.before_widget }}}
+<?php echo $args['before_widget']; ?>
 
-	<div class="number-counters" data-speed="{{ instance.speed }}">
-	{{# counters }}
+	<div class="number-counters" data-speed="<?php echo esc_attr( $instance['speed'] ); ?>">
+	<?php foreach ( $counters as $counter ) : ?>
 		<div class="number-counter">
-			{{# icon }}
-			<i class="number-counter__icon  fa  {{ icon }}"></i>
-			{{/ icon }}
-			<div class="number-counter__number  js-number" data-to="{{ number }}">{{ leading_zeros }}</div>
-			<div class="number-counter__title">{{ title }}</div>
+			<?php if ( ! empty( $counter['icon'] ) ) : ?>
+				<i class="number-counter__icon  fa  <?php echo esc_attr( $counter['icon'] ); ?>"></i>
+			<?php endif; ?>
+			<div class="number-counter__number  js-number" data-to="<?php echo absint( $counter['number'] ); ?>"><?php echo esc_html( PW_Functions::leading_zeros( strlen( $counter['number'] ) ) ); ?></div>
+			<div class="number-counter__title"><?php echo esc_html( $counter['title'] ); ?></div>
 		</div>
-	{{/ counters }}
+	<?php endforeach; ?>
 	</div>
 
-{{{ args.after_widget }}}
+<?php echo $args['after_widget']; ?>
