640b2ea5389997dd2b60d5442cd68f7cfa3850ff
Gregor Capuder <capuderg@gmail.com>
2015-12-04 15:40:57 +0100

    Updated Accordion View part 2.

--- widgets/views/widget-accordion.php
+++ widgets/views/widget-accordion.php
@@ -8,14 +8,14 @@
 	<div class="accordion panel-group" id="accordion-<?php echo esc_attr( $args['widget_id'] ); ?>" role="tablist" aria-multiselectable="true">
 	<?php foreach ( $items as $item ) : ?>
 		<div class="accordion__panel  panel  panel-default">
-			<div class="accordion__heading  panel-heading" role="tab" id="heading-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['key'] ); ?>">
+			<div class="accordion__heading  panel-heading" role="tab" id="heading-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['id'] ); ?>">
 				<h4 class="panel-title">
-					<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion-<?php echo esc_attr( $args['widget_id'] ); ?>" href="#collapse-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['key'] ); ?>" aria-expanded="false" aria-controls="collapse-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['key'] ); ?>">
+					<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion-<?php echo esc_attr( $args['widget_id'] ); ?>" href="#collapse-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['id'] ); ?>" aria-expanded="false" aria-controls="collapse-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['id'] ); ?>">
 						<?php echo wp_kses_post( $item['title'] ); ?>
 					</a>
 				</h4>
 			</div>
-			<div id="collapse-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['key'] ); ?>" class="accordion__content  panel-collapse  collapse" role="tabpanel" aria-labelledby="heading-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['key'] ); ?>">
+			<div id="collapse-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['id'] ); ?>" class="accordion__content  panel-collapse  collapse" role="tabpanel" aria-labelledby="heading-<?php echo esc_attr( $args['widget_id'] ) . '-' . esc_attr( $item['id'] ); ?>">
 				<div class="panel-body">
 					<?php echo wp_kses_post( $item['content'] ); ?>
 				</div>
