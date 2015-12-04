654d3818f24a85cb4b2812aa35306116e9421171
Gregor Capuder <capuderg@gmail.com>
2015-12-04 14:56:44 +0100

    Updated Skype view.

--- widgets/views/widget-skype.php
+++ widgets/views/widget-skype.php
@@ -1,8 +1,8 @@
-<?php echo $args['before_widget'] ?>
+<?php echo $args['before_widget']; ?>
 
-	<a class="skype-button" href="<?php echo $this->e( $instance['skype_username'], 'esc_attr' ) ?>">
-		<i class="fa  fa-<?php echo $this->e( $instance['icon'], 'esc_attr' ) ?>"></i>
-		<p class="skype-button__title"><?php echo $this->e( $instance['title'], 'wp_kses_post' ) ?></p>
+	<a class="skype-button" href="<?php echo esc_attr( $instance['skype_username'] ); ?>">
+		<i class="fa  fa-<?php echo esc_attr( $instance['icon'] ); ?>"></i>
+		<p class="skype-button__title"><?php echo wp_kses_post( $instance['title'] ); ?></p>
 	</a>
 
-<?php echo $args['after_widget'] ?>
+<?php echo $args['after_widget']; ?>
