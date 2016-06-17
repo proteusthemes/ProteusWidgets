58a1897f62649b19115f14a78f8a2068bde4c00d
Gregor Capuder <capuderg@gmail.com>
2016-06-17 09:41:51 +0200

    Bumped plugin version to 1.0.9.

--- proteuswidgets.php
+++ proteuswidgets.php
@@ -3,7 +3,7 @@
 Plugin Name: ProteusWidgets
 Plugin URI: http://www.proteusthemes.com
 Description: WP widgets for retail businesses by ProteusThemes
-Version: 1.0.8
+Version: 1.0.9
 Author: ProteusThemes
 Author URI: http://www.proteusthemes.com
 License: GPL3
