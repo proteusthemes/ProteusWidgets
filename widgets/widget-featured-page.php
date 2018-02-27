f9a5379556474ab77ad28585a04f0e835b7357fc
Gorazd Krumpak <gorazd@primaprodukcija.si>
2018-02-27 08:52:04 +0100

    Change instruction text

--- widgets/widget-featured-page.php
+++ widgets/widget-featured-page.php
@@ -157,7 +157,7 @@ if ( ! class_exists( 'PW_Featured_Page' ) ) {
 					) );
 				?>
 				<br>
-				<small><?php esc_html_e( 'Edit page:', 'proteuswidgets' ); ?> <a id="<?php echo esc_attr( $this->get_field_id( 'page_id' ) ); ?>-edit-link" href="" target="_blank"><?php echo esc_attr( $this->get_field_id( 'page_id' ) ); ?></a></small>
+				<small><?php esc_html_e( 'Edit selected fetured page text and image:', 'proteuswidgets' ); ?> <a id="<?php echo esc_attr( $this->get_field_id( 'page_id' ) ); ?>-edit-link" href="" target="_blank"></a></small>
 			</p>
 
 			<p>
